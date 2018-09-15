99906be2a00e68a895471faf6b54649ddfe3bf4b
ruszkipista <16229509@studentmail.ul.ie>
2018-09-15 08:09:30 +0100

    added activity

--- app/src/main/java/io/ruszkipista/hellobutton/MainActivity.java
+++ app/src/main/java/io/ruszkipista/hellobutton/MainActivity.java
@@ -1,13 +1,34 @@
 package io.ruszkipista.hellobutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int mCounter = 0;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text_count);
+        mTextView.setText(getString(R.string.message_format,mCounter));
+
+    }
+
+    public void increase_counter(View view){
+        mCounter++;
+        updateView();
+    }
+    public void decrease_counter(View view){
+        mCounter--;
+        updateView();
+    }
+
+    private void updateView(){
+        mTextView.setText(getString(R.string.message_format,mCounter));
     }
 }
